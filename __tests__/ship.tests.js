59ab508b7cc166adc563d797f985c0acfc0304c2
Tom Bilbe <tbilbe@gmail.com>
2019-03-11 22:00:24 +0000

    update to ship and ship tests - dependency inversion

--- __tests__/ship.tests.js
+++ __tests__/ship.tests.js
@@ -6,9 +6,7 @@ const { Port } = require('../src/port.js');
 const { Itinerary } = require('../src/itinerary.js');
 
 describe('A ship was built and christened', () => {
-  let ship;
-  let port;
-  let itinerary;
+  let ship, port, itinerary;
   test('Before a ship sails it must have a port', () => {
     port = new Port('Dover');
     itinerary = new Itinerary(port);
@@ -18,9 +16,7 @@ describe('A ship was built and christened', () => {
 });
 
 describe('Setting Sail', () => {
-  let rowBoat;
-  let mockPort;
-  let itinerary;
+  let rowBoat, mockPort, itinerary;
   test('Ship has to leave a port to set sail', () => {
     mockPort = new Port('Dover');
     itinerary = new Itinerary(mockPort);
@@ -34,14 +30,16 @@ describe('Ships can dock at different Ports', () => {
   let port1, port2, bigBoat, itinerary;
   test('The port object is instantiated', () => {
     port1 = new Port('port1');
-    itinerary = new Itinerary(port1);
+    port2 = new Port('kingston upon hull');
+    itinerary = new Itinerary([port1, port2]);
     bigBoat = new Ship(itinerary);
     expect(bigBoat.currentPort).toBe(port1);
-    port2 = new Port('kingston upon hull');
     bigBoat.setSail();
-    bigBoat.dock(port2);
-    console.log('bigBoat: ', bigBoat);
+    console.log('bigBoat currentport should be false: ', bigBoat.currentPort);
+    bigBoat.dock();
+    console.log('after docking in port 2, check port 2 logs: ', port2.ships);
     expect(bigBoat.currentPort).toBe(port2);
+    expect(port2.ships).toContain(bigBoat);
   });
 });
 
--- src/ship.js
+++ src/ship.js
@@ -10,8 +10,11 @@ Ship.prototype.setSail = function () {
   this.previousPort = this.itinerary.ports[0].name;
 };
 
-Ship.prototype.dock = function (port) {
-  this.currentPort = port;
+Ship.prototype.dock = function () {
+  const itinerary = this.itinerary;
+  const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+  this.currentPort = itinerary.ports[previousPortIndex + 1];
+  this.currentPort.addShip(this);
 };
 
 
